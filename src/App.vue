25e11e17fff4a0edfb71e0e50b7aec979d480a91
Mưu Văn Duy <duymv@kaopiz.com>
2020-10-01 10:39:25 +0900

    Add Rating controller

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RatingController vue_rating="Vue Rating" username="Jonny" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import RatingController from "./components/RatingController.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    RatingController
   }
 };
 </script>
